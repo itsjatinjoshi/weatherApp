59f7fbabd044079da116bc9027107039745cf184
1893888 <jatinjoshi2283@gmail.com>
2019-10-15 17:30:45 -0400

    spalsh remaining

---
+++ app/src/main/java/com/example/weatherapp/Retrofit/Splash.java
@@ -0,0 +1,50 @@
+package com.example.weatherapp.Retrofit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.example.weatherapp.MainActivity;
+import com.example.weatherapp.R;
+
+public class Splash extends AppCompatActivity {
+    ImageView Welcome_Logo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ActionBar actionBar = getSupportActionBar();
+        getSupportActionBar().setTitle("Welcome to Food NutrI");
+        actionBar.setSubtitle("You can search best food here");
+
+        Welcome_Logo= findViewById(R.id.Welcome_Logo);
+
+
+        Animation ani = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate);
+        ani.setDuration(3000);
+
+        Welcome_Logo.setAnimation(ani);
+
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate);
+        animation.setDuration(3000);
+        Welcome_Logo.setAnimation(animation);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this, MainActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        },10000);
+
+    }
+}
